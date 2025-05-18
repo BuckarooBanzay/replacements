19a5aed0b808b470b253ee61d249361fb097bacf
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-05-18 21:41:42 +0200

    add `mapcolor` attribute to nodes

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ for i=0,9 do
     minetest.register_node("replacements:replacement_" .. i, {
         description = "Replacement block #" .. i,
         tiles = {"ehlphabet_0" .. (i+48) .. ".png"},
+        mapcolor = { r = 255, g = 255, b = 255 },
         groups = {
             dig_immediate = 2
         }
@@ -12,6 +13,7 @@ for i=0,9 do
     stairsplus:register_all("replacements", "replacement_" .. i,  "replacements:replacement_" .. i, {
         description = "Replacement block #" .. i,
         tiles = {"ehlphabet_0" .. (i+48) .. ".png"},
+        mapcolor = { r = 255, g = 255, b = 255 },
         groups = {
             dig_immediate = 2
         }
